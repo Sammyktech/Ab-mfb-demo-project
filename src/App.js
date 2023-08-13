ea8b06ea5f9d2c43caf9fdc6683de2b316de9a75
unknown <oloweayodele09@gmail.com>
2023-08-13 14:08:15 +0100

    feat:change in Appjs

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import Main from './Component/Main'
 import Section from './Component/Section'
 import Footers from './Component/Footers'
 import "./App.css"
-import {BrowserRouter, Routes,Route } from 'react-router'
-// import {Link} from 'react-router-dom'
+
+
 function App() {
   return (
     <div className=''>
-      {/* <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Hero/>}></Route>
-        <Route path='/Main' element={<Main/>}></Route>
-        <Route path='/Section' element={<Section/>}></Route>
-        <Route path='/Footers' element={<Footers/>}></Route> */}
-       
-      {/* </Routes>
-      </BrowserRouter> */}
+      
       <Hero/>
       <Logo/>
       <Main/>
